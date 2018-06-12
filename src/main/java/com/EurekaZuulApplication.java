f788f60efba2c5433f598e4ee23a4f7196d48fd0
chscls@163.com <chscls@163.com>
2018-06-12 10:55:17 +0800

    1111111111

--- src/main/java/com/EurekaZuulApplication.java
+++ src/main/java/com/EurekaZuulApplication.java
@@ -1,8 +1,12 @@
 package com;
 
+import java.io.File;
+
 import javax.servlet.MultipartConfigElement;
 
 import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,7 +23,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -35,11 +39,38 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableHystrixDashboard
 @SpringBootApplication
 public class EurekaZuulApplication {
-	
+	@Autowired
+	private Environment env;
 	public static void main(String[] args) {
 			SpringApplication.run(EurekaZuulApplication.class, args);
 	
 	}
+	 @Bean
+	    public EmbeddedServletContainerFactory servletContainer() {
+	        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+	        tomcat.addAdditionalTomcatConnectors(createSslConnector()); // 添加http
+	        return tomcat;
+	    }
+
+	 private Connector createSslConnector() {
+	        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+	        String x = env.getProperty("https.file");
+	        String y = env.getProperty("https.password");
+	        String p = env.getProperty("https.port");
+	            File keystore = new File(x);
+	            /*File truststore = new ClassPathResource("sample.jks").getFile();*/
+	            connector.setScheme("https");
+	            connector.setSecure(true);
+	            connector.setPort(Integer.parseInt(p));
+	            protocol.setSSLEnabled(true);
+	            protocol.setKeystoreFile(keystore.getAbsolutePath());
+	            protocol.setKeystorePass(y);
+	           // protocol.setKeyPass(key_password);
+	            return connector;
+	      
+	     
+	    }
 	 
 	
 }
