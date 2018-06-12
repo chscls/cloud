df2d595a0c771138bd632eeb9c70d827a9c78823
chscls@163.com <chscls@163.com>
2018-06-12 09:25:07 +0800

    111111111111

---
+++ src/main/java/com/Application.java
@@ -0,0 +1,44 @@
+package com;
+
+import javax.servlet.MultipartConfigElement;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+
+
+@SpringBootApplication
+@ServletComponentScan
+/*@EnableEurekaClient*/
+@EnableZuulProxy
+
+@EnableEurekaServer
+@EnableCircuitBreaker
+public class Application {
+	
+	public static void main(String[] args) {
+			SpringApplication.run(Application.class, args);
+	
+	}
+	 
+	
+}
